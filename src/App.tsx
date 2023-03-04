1b31d29efbd8d81e277d075852e57acc69572c7b
muhammadfajri-student-devs <105767911+muhammadfajri-student-devs@users.noreply.github.com>
2023-03-04 22:18:47 +0700

    setup routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import "./App.css";
 
 import * as React from "react";
 
-import useConfig from "./components/useConfig";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Create from "./components/Create";
+import NoteDetails from "./components/NoteDetails";
 
 /**
  * Our Web Application
  */
 export default function App() {
-  const config = useConfig();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/notes/:id">
+            <NoteDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
