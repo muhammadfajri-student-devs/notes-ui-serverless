a40425c29bb7b9486fe2f0da9d8e4b61aa29ea56
muhammadfajri-student-devs <105767911+muhammadfajri-student-devs@users.noreply.github.com>
2023-03-04 22:13:10 +0700

    add support for react router

--- src/browser/index.tsx
+++ src/browser/index.tsx
@@ -9,6 +9,7 @@ import "./index.css";
  */
 import * as React from "react";
 import { hydrate } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import ConfigContext from "../components/ConfigContext";
 import { Config } from "../server/config";
@@ -17,13 +18,17 @@ import App from "../App";
 const config = (window as any).__CONFIG__ as Config;
 delete (window as any).__CONFIG__;
 
+const basename = config.app.URL.match(/^(?:https?:\/\/)?[^\/]+(\/?.+)?$/i)?.[1];
+
 /** Components added here will _only_ be loaded in the web browser, never for server-side rendering */
 const render = () => {
   hydrate(
     <>
       {/* The configuration is the outmost component. This allows us to read the configuration even in the theme */}
       <ConfigContext.Provider value={config}>
-        <App />
+        <BrowserRouter basename={basename}>
+          <App />
+        </BrowserRouter>
       </ConfigContext.Provider>
     </>,
     document.getElementById("root"),
