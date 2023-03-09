51b5bfd68464a76c9021e75f28294c5937e82d55
muhammadfajri-student-devs <105767911+muhammadfajri-student-devs@users.noreply.github.com>
2023-03-09 16:49:06 +0700

    update/edit data

--- src/components/NoteDetails.tsx
+++ src/components/NoteDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useHistory, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
@@ -6,6 +6,10 @@ import useConfig from "./useConfig";
 
 const NoteDetails = () => {
   const { id } = useParams<any>();
+
+  const [edit, setEdit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const { app } = useConfig();
   const url = app.NOTES_API_URL;
 
@@ -15,15 +19,45 @@ const NoteDetails = () => {
 
   const { data: notes, error, isPending } = useFetch(`${url}/notes/` + id);
 
+  const [title, setTitle] = useState(notes?.title);
+  const [desc, setDesc] = useState(notes?.desc);
+  const [priority, setPriority] = useState(notes?.priority);
+
   const history = useHistory();
 
   // * Delete notes and redirect the user to home page once they've delete it
   const handleClick = () => {
-    fetch(`${url}/notes/` + notes!.id, {
+    fetch(`${url}/notes/${notes!.id}`, {
       method: "DELETE",
     }).then(() => {
       history.push("/");
     });
+  };
+
+  // * Edit notes
+  const handleEdit = (e: any) => {
+    e.preventDefault();
+
+    const note = { title, desc, priority };
+    setIsLoading(true);
+
+    console.log(note);
+
+    try {
+      fetch(`${url}/notes/${notes!.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note),
+      }).then(() => {
+        console.log("New note added");
+        setIsLoading(false);
+        history.push("/");
+      });
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
   };
 
   return (
@@ -37,8 +71,33 @@ const NoteDetails = () => {
           <p>
             Priority Type: <b>{notes.priority}</b>
           </p>
+          <button style={{ background: "mediumspringgreen", color: "black" }} onClick={() => setEdit(!edit)}>
+            Edit
+          </button>
           <button onClick={handleClick}>Delete</button>
         </article>
+      )}
+      {notes && edit && (
+        <div className="create" style={{ margin: "20px 0" }}>
+          <form onSubmit={handleEdit}>
+            <label>Note title:</label>
+            <input type="text" defaultValue={notes.title} onChange={(e) => setTitle(e.target.value)} />
+            <label>Note body:</label>
+            <textarea defaultValue={notes.desc} onChange={(e) => setDesc(e.target.value)}></textarea>
+            <label>Note priority:</label>
+            <select defaultValue={notes.priority} onChange={(e) => setPriority(e.target.value)}>
+              <option value="normal">Normal</option>
+              <option value="mandatory">Mandatory</option>
+              <option value="urgent">Urgent</option>
+            </select>
+            {!isLoading && <button style={{ background: "mediumspringgreen", color: "black" }}>Update Note</button>}
+            {isLoading && (
+              <button style={{ background: "mediumspringgreen", color: "black" }} disabled>
+                Updating Note...
+              </button>
+            )}
+          </form>
+        </div>
       )}
     </div>
   );
